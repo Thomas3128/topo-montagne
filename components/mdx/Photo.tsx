57d283fc03e8262797742b83a432fb4a53d6ae55
Thomas Dijs <Thomas.DIJS@3ds.com>
2026-06-03 20:05:37 +0200

    Pictures sizes

--- components/mdx/Photo.tsx
+++ components/mdx/Photo.tsx
@@ -2,11 +2,15 @@ interface Props {
   src: string;
   caption?: string;
   alt?: string;
+  width?: number; // pourcentage de la largeur du contenu, ex: 50
 }
 
-export default function Photo({ src, caption, alt }: Props) {
+export default function Photo({ src, caption, alt, width }: Props) {
   return (
-    <figure className="mdx-photo">
+    <figure
+      className="mdx-photo"
+      style={width ? { width: `${width}%`, marginLeft: 'auto', marginRight: 'auto' } : undefined}
+    >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src={src} alt={alt ?? caption ?? ''} className="mdx-photo-img" />
       {caption && <figcaption className="mdx-photo-caption">{caption}</figcaption>}
